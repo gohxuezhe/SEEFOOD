74ac3160d9cfe670bfae42541c462d781d313c5e
Goh Xue Zhe <gohxuezhe@gmail.com>
2023-06-19 22:10:29 +0800

    design layout

---
+++ app/src/main/java/com/example/seefood/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.seefood;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+    private boolean onTap = false;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
