dc41bfe98b11c2b52651dea97283a3ccf772cbee
Goh Xue Zhe <gohxuezhe@gmail.com>
2023-06-19 23:29:15 +0800

    update MainActivity

--- app/src/main/java/com/example/seefood/MainActivity.java
+++ app/src/main/java/com/example/seefood/MainActivity.java
@@ -1,15 +1,98 @@
 package com.example.seefood;
 
+import static android.view.View.INVISIBLE;
+import static android.view.View.VISIBLE;
+
+import android.Manifest;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.os.Bundle;
+import android.provider.MediaStore;
+import android.widget.Button;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import java.io.FileOutputStream;
 
 public class MainActivity extends AppCompatActivity {
-    private boolean onTap = false;
+    RelativeLayout layout;
+    TextView get_started;
+    TextView touch_to_seefood;
+    Button camera_button;
+    Bitmap bitmap;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        getPermission();
+
+        layout = findViewById(R.id.layout);
+        get_started = findViewById(R.id.get_started);
+        camera_button = findViewById(R.id.camera_button);
+        touch_to_seefood = findViewById(R.id.touch_to_seefood);
+
+        layout.setOnClickListener(view -> {
+            get_started.setVisibility(INVISIBLE);
+            camera_button.setVisibility(VISIBLE);
+            touch_to_seefood.setVisibility(VISIBLE);
+        });
+
+        camera_button.setOnClickListener(view -> {
+            Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+            startActivityForResult(intent, 10);
+        });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        if (requestCode == 10) {
+            if (data != null) {
+                bitmap = (Bitmap) data.getExtras().get("data");
+                try {
+                    // Write file
+                    String filename = "bitmap.png";
+                    FileOutputStream stream = this.openFileOutput(filename, Context.MODE_PRIVATE);
+                    bitmap.compress(Bitmap.CompressFormat.PNG, 100, stream);
+
+                    // Cleanup
+                    stream.close();
+                    bitmap.recycle();
+
+                    // Pop intent
+                    Intent intent = new Intent(this, ResultsActivity.class);
+                    intent.putExtra("bitmap", filename);
+                    startActivity(intent);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if (requestCode == 11) {
+            if (grantResults.length > 0) {
+                if (grantResults[0] != PackageManager.PERMISSION_GRANTED) {
+                    getPermission();
+                }
+            }
+        }
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+    }
+
+    private void getPermission() {
+        if (checkSelfPermission(Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+            requestPermissions(new String[]{Manifest.permission.CAMERA}, 11);
+        }
     }
 }
--- app/src/main/java/com/example/seefood/ResultsActivity.java
+++ app/src/main/java/com/example/seefood/ResultsActivity.java
@@ -1,13 +1,32 @@
 package com.example.seefood;
 
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
+import android.widget.ImageView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import java.io.FileInputStream;
+
 public class ResultsActivity extends AppCompatActivity {
+    Bitmap bitmap;
+    ImageView imageView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_results);
+
+        String filename = getIntent().getStringExtra("bitmap");
+        try {
+            FileInputStream stream = this.openFileInput(filename);
+            bitmap = BitmapFactory.decodeStream(stream);
+            stream.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        imageView = findViewById(R.id.picture_taken);
+
+        imageView.setImageBitmap(bitmap);
     }
 }
